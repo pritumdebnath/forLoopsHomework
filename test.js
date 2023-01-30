8c77ee75092b4b06e71962ad0d40252388c370ac
pritumdebnath <122291993+pritumdebnath@users.noreply.github.com>
2023-01-30 17:45:37 -0500

    new push

---
+++ test.js
@@ -0,0 +1,21 @@
+var forArray = [];
+for (var i = 1; i <= 10; i++){
+    forArray.push(i);
+}
+
+var whileArray = [];
+var i = 1;
+while (i <= 10){
+    whileArray.push(i);
+    i++;
+}
+
+var oddArray = [];
+for (let i = 11; i<=21; i+=2){
+    oddArray.push(i);
+}
+
+var decrementArray = [];
+for (let i = 15; i >= 1; i-=2){
+    decrementArray.push(i);
+}
